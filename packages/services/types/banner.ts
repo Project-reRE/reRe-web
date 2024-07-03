8d161c7d9c444e5248a66866863daa074c9a54e5
Eunbin Kang <kub0803@naver.com>
2024-07-04 02:49:03 +0900

    feat: add banner dto

---
+++ packages/services/types/banner.ts
@@ -0,0 +1,10 @@
+export interface OpenBannerResponseDto {
+  title: string;
+  body: string;
+  template: string;
+  route: string;
+  boxHexCode: string;
+  displayOrder: number;
+  imageUrl: string;
+  display: boolean;
+}
