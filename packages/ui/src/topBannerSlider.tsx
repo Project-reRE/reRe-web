46bc4d280c55ebc308144ed2ac610c295b5caaa3
Eunbin Kang <kub0803@naver.com>
2024-07-22 01:27:33 +0900

    feat: TopBannerSlider

--- packages/ui/src/topBannerSlider.tsx
+++ packages/ui/src/topBannerSlider.tsx
@@ -3,38 +3,46 @@
 import 'keen-slider/keen-slider.min.css';
 import { useKeenSlider } from 'keen-slider/react';
 import '../index.css';
+import { useState } from 'react';
 
 type Props = {
   items: any;
 };
 
 const TopBannerSlider = ({ items }: Props) => {
-  const [sliderRef, instanceRef] = useKeenSlider({
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [sliderRef] = useKeenSlider({
     loop: true,
     drag: true,
-    slideChanged() {
-      console.log('slide changed');
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
     },
   });
 
   return (
-    <>
-      <div className="h-full max-h-[340px] w-full">This div has a max height of 340px and full width</div>
-      <div ref={sliderRef} className="h-full max-h-[340px] w-[30px]">
+    <div className="relative">
+      <div ref={sliderRef} className="keen-slider">
         {items.map((item: any) => (
-          <div
-            key={item.id}
-            className="keen-slider__slide flex h-full max-h-[340px] w-full items-center justify-center"
-          >
-            <img
-              className="h-full max-h-[340px]"
-              src="https://s3-alpha-sig.figma.com/img/5fb5/b9b4/efb9dc95a764f5f33d5f427c92e0ff33?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=o5~9bpeYDx5xlQZ7kxE06ZPobV~JFvFMTfCxwPeOl36uPxNfQYZel~RxqtpbJg8tajKc8K6YxK1MkXBEF-T6pmLsXNwKSiPyz2PgXYW6aQqeopCBFjkA8zth~6jz8ctC9URZV4jGZ6s7vaqEha7hP3PYKlc6F6UX6yAJhfHlN5qZ3IFRyvKsPXJ1x3-QUw5ZdEUghYs9VS7zUKkRlij9ASyNi2JroDgD7kKpdGVmIQBfUNrb0pqvcwAneI3zFO3aOHIe~4k1U39YMr4VIhzSrvzzwRyhL4ErkkmhEj0pjxQMyfurzLnz7BsIgfDnP2c7WyLD6RkOEhboEL6~lWkXaw__"
-              alt="아수라"
-            />
+          <div key={item.id} className="keen-slider__slide flex w-full items-center justify-center">
+            <img className="max-h-[552px] w-full object-cover" src={item.imageUrl} alt="아수라" />
           </div>
         ))}
       </div>
-    </>
+      <div className="absolute bottom-[22px] left-1/2 flex -translate-x-1/2 transform gap-[8px]">
+        {items.map((item: any, index: number) => (
+          <div
+            key={item.id}
+            className={
+              'bg-Gray80 h-[6px] w-[6px] rounded-full duration-300 ease-in-out' +
+              (currentSlide === index ? ' w-[24px] bg-white' : '')
+            }
+          />
+        ))}
+      </div>
+      {/* dim */}
+      <div className="to-Gray10 absolute top-0 h-[90px] w-full bg-gradient-to-t from-transparent" />
+      <div className="to-Gray10 absolute bottom-0 h-[50px] w-full bg-gradient-to-b from-transparent" />
+    </div>
   );
 };
 
