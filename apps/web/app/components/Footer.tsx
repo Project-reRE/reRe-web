c5c1b8b5d2e05f06af30b34b7ca95c24d971aa84
Eunbin Kang <kub0803@naver.com>
2024-08-22 22:16:37 +0900

    feat: 푸터 운영 정책 추가

---
+++ apps/web/app/components/Footer.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { LINK } from '../constant/link';
+
+const Footer = () => {
+  return (
+    <div className="mt-[140px] flex w-full flex-col items-center justify-center gap-10 bg-[#141414] pb-10">
+      <div className="h-px self-stretch bg-[#444444]" />
+      <div className="flex h-[84px] flex-col items-center justify-start gap-3 self-stretch">
+        <ul className="flex items-start justify-start gap-4">
+          <li className="text-Gray70 text-sm font-normal leading-tight">
+            <a href={LINK.termsOfService}>서비스 이용약관</a>
+          </li>
+          <li className="text-Gray70 text-sm font-normal leading-tight">
+            <a href={LINK.privacyPolicy}>개인정보처리방침</a>
+          </li>
+          <li className="text-Gray70 text-sm font-normal leading-tight">
+            <a href={LINK.operatingPolicy}>운영정책</a>
+          </li>
+        </ul>
+        <a href="mailto: pevelopment@gmail.com" className="text-Gray70 text-sm font-normal leading-tight">
+          문의: pevelopment@gmail.com
+        </a>
+        <p className="text-Gray70 text-sm font-normal leading-tight">Copyright © reRE 2024. All Rights Reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ apps/web/app/constant/link.ts
@@ -0,0 +1,5 @@
+export const LINK = {
+  termsOfService: 'https://revaluation.notion.site/61bff9fc064047dbaeec12e2c792b526?pvs=74',
+  privacyPolicy: 'https://revaluation.notion.site/3e61d87145254fc6b8cf02b7853304d7?pvs=74',
+  operatingPolicy: 'https://revaluation.notion.site/72ff79808476490aa5feb170caa59652',
+};
