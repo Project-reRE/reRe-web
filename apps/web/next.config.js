f2e262dc94bddef79e92220c1631fc0b1d59eaed
Eunbin Kang <kub0803@naver.com>
2024-08-22 22:59:29 +0900

    feat: image remotePatterns 외부 이미지 허용

---
+++ apps/web/next.config.js
@@ -0,0 +1,49 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'file.koreafilm.or.kr',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+
+    if (isServer) {
+      // next server build => ignore msw/browser
+      if (Array.isArray(config.resolve.alias)) {
+        // in Next the type is always object, so this branch isn't necessary. But to keep TS happy, avoid @ts-ignore and prevent possible future breaking changes it's good to have it
+        config.resolve.alias.push({ name: 'msw/browser', alias: false });
+      } else {
+        config.resolve.alias['msw/browser'] = false;
+      }
+    } else {
+      // browser => ignore msw/node
+      if (Array.isArray(config.resolve.alias)) {
+        config.resolve.alias.push({ name: 'msw/node', alias: false });
+      } else {
+        config.resolve.alias['msw/node'] = false;
+      }
+    }
+    return config;
+  },
+
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+};
