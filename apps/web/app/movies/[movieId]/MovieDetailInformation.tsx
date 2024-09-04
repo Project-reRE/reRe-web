552cb3fa8462f0b561c78b20dc2eac1aeddb2477
Eunbin Kang <kub0803@naver.com>
2024-09-05 01:06:28 +0900

    feat: 영화 상세 API 연동 및 달력 이동 기능 추가

--- apps/web/app/movies/[movieId]/MovieDetailInformation.tsx
+++ apps/web/app/movies/[movieId]/MovieDetailInformation.tsx
@@ -1,18 +1,23 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 
 import { useParams, useRouter, useSearchParams } from 'next/navigation';
 
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
-import { format } from 'date-fns';
+import { addMonths, format, subMonths } from 'date-fns';
 
 import { ArrowIcon } from '@repo/icon';
 import { MovieResponseDto, useGetRevaluations } from '@repo/services';
+import EmotionRankCard from '@repo/ui/EmotionRankCard';
+import PieCharts from '@repo/ui/PieCharts';
 import ReviewCard from '@repo/ui/ReviewCard';
 import SimpleAreaChart from '@repo/ui/SimpleAreaChart';
 import SimpleBarChart from '@repo/ui/SimpleBarChart';
+import ValuationTotalCard from '@repo/ui/ValuationTotalCard';
+
+import { convertAgeTypeToKeyValueObjectArray } from 'utils/convertKeyValueObjectArray';
 
 import EmptyBlankView from 'components/EmptyBlankView';
 import ReportModalBody from 'components/ReportModalBody';
@@ -33,10 +38,14 @@ const MovieDetailInformation = ({ data }: Props) => {
   const router = useRouter();
   const params = useParams();
 
-  const formattedDate = format(new Date(), 'yyyy-MM');
-  const [monthData, setMonthData] = useState(formattedDate);
-
   const { data: revaluationsData } = useGetRevaluations({ movieId: data.id });
+
+  const currentDate = format(new Date(), 'yyyy-MM');
+  const [monthData, setMonthData] = useState(currentDate);
+  const targetStatistics = useMemo(() => data.statistics.find((el) => el.currentDate === monthData), [data, monthData]);
+  const isNextButton = useMemo(() => !(currentDate <= monthData), [monthData]);
+  console.log(data);
+
   // const [reviewResults, setReviewResults] = useState(data.)
 
   const handleChange = (_: React.SyntheticEvent, value: TabType) => {
@@ -47,37 +56,90 @@ const MovieDetailInformation = ({ data }: Props) => {
     router.push(PATH.MOVIES + `/${params.movieId}/revaluation`);
   };
 
+  const handleClickPrevDate = useCallback(() => {
+    const date = format(subMonths(monthData, 1), 'yyyy-MM');
+    setMonthData(date);
+  }, [monthData]);
+
+  const handleClickNextDate = useCallback(() => {
+    if (!isNextButton) return;
+    const date = format(addMonths(monthData, 1), 'yyyy-MM');
+    setMonthData(date);
+  }, [monthData]);
+
+  const handleClickGoMovieButton = () => {
+    window.open(`https://serieson.naver.com/v3/search?query=${data.data.title}`);
+  };
+
   return (
     <>
       <div className="flex h-11 w-[1152px] items-center justify-center gap-5 bg-gradient-to-t px-[100px]">
-        <button>
+        <button onClick={handleClickPrevDate}>
           <ArrowIcon />
         </button>
         <div className="text-3xl font-bold text-white">{monthData}</div>
-        <button className="rotate-180">{/* <ArrowIcon onClick={()=> setMonthData(0)} /> */}</button>
+        <button className="w-[24px] rotate-180" onClick={handleClickNextDate} disabled={!isNextButton}>
+          {isNextButton && <ArrowIcon />}
+        </button>
       </div>
       <div className="container flex flex-col items-center gap-[32px] px-[100px]">
         <Tabs value={tabState} onChange={handleChange} className="w-full">
           <Tab label="재평가 정보" value={'info'} sx={{ width: 80, padding: '13px 0 10px' }} disableRipple />
           <Tab label="다른 평보기" value={'review'} sx={{ width: 80, padding: '13px 0 10px' }} disableRipple />
         </Tabs>
-        <TabPanel value={tabState} active={'info'}>
-          <SimpleAreaChart />
-          <SimpleBarChart />
-          {/* <EmptyBlankView title="2024년 7월에는 등록된 재평가가 없어요." description="가장 먼저 평가를 남겨보세요!" /> */}
-        </TabPanel>
-        <TabPanel value={tabState} active={'review'} className="grid grid-cols-2 gap-[16px]">
-          <ReviewCard />
-          <ReviewCard />
-          {revaluationsData?.results.map((item) => <ReviewCard item={item} key={item.id} />)}
-        </TabPanel>
+        {targetStatistics ? (
+          <>
+            <TabPanel value={tabState} active={'info'} className="flex flex-col items-center gap-[32px]">
+              <div className="flex w-full flex-wrap gap-[10px]">
+                <SimpleAreaChart data={targetStatistics.numRecentStars} />
+                <SimpleBarChart data={[]} />
+                <EmotionRankCard
+                  title="개봉 당시, 이 영화에 대해서 사람들은요"
+                  data={targetStatistics.numPastValuation}
+                />
+                <EmotionRankCard
+                  title="7월에 이 영화에 대해서 사람들은요"
+                  data={targetStatistics.numPresentValuation}
+                />
+              </div>
+              <button
+                className="h-14 w-[341px] rounded-[10px] bg-[#749037] text-center text-2xl font-medium text-white"
+                onClick={handleClickGoMovieButton}
+              >
+                영화 보러가기
+              </button>
+              <div className="flex w-full flex-wrap gap-[10px]">
+                <ValuationTotalCard />
+                <PieCharts data={convertAgeTypeToKeyValueObjectArray(targetStatistics.numAge)} />
+              </div>
+            </TabPanel>
+            <TabPanel value={tabState} active={'review'} className="grid grid-cols-2 gap-[16px]">
+              <ReviewCard />
+              <ReviewCard />
+              {/* {revaluationsData?.results.map((item) => <ReviewCard item={item} key={item.id} />)} */}
+            </TabPanel>
+            <button
+              className="bg-Orange50 h-14 w-[341px] rounded-[10px] text-center text-2xl font-medium text-white"
+              onClick={handleClickRevaluation}
+            >
+              재평가하기
+            </button>
+          </>
+        ) : (
+          <EmptyBlankView
+            title={`${format(monthData, 'yyyy')}년 ${format(monthData, 'M')}월에는 등록된 재평가가 없어요.`}
+            description="가장 먼저 평가를 남겨보세요!"
+            children={
+              <button
+                className="bg-Orange50 mt-[48px] h-14 w-[341px] rounded-[10px] text-center text-2xl font-medium text-white"
+                onClick={handleClickRevaluation}
+              >
+                재평가하기
+              </button>
+            }
+          />
+        )}
 
-        <button
-          className="bg-Orange50 h-14 w-[341px] rounded-[10px] text-center text-2xl font-medium text-white"
-          onClick={handleClickRevaluation}
-        >
-          재평가하기
-        </button>
         <ReportModalBody />
       </div>
     </>
