cf3da5975df82284f34a4a14eb2f88af49084022
Eunbin Kang <kub0803@naver.com>
2024-09-05 01:07:29 +0900

    feat: Positive, Negative, NotSure 이모지 추가

--- packages/icon/src/index.ts
+++ packages/icon/src/index.ts
@@ -4,6 +4,9 @@ import GoogleSocialIcon from './GoogleSocialIcon.tsx';
 import InfoIcon from './InfoIcon.tsx';
 import KakaoSocialIcon from './KakaoSocialIcon.tsx';
 import LogoIcon from './LogoIcon.tsx';
+import NegativeEmoji from './NegativeEmoji.tsx';
+import NotSureEmoji from './NotSureEmoji.tsx';
+import PositiveEmoji from './PositiveEmoji.tsx';
 import Ranking1 from './Ranking1.tsx';
 import Ranking2 from './Ranking2.tsx';
 import Ranking3 from './Ranking3.tsx';
@@ -22,4 +25,7 @@ export {
   ArrowIcon,
   StarIcon,
   InfoIcon,
+  NegativeEmoji,
+  NotSureEmoji,
+  PositiveEmoji,
 };
