ba06b3149a32932c5cabb0542dcea53b852718e2
Eunbin Kang <kub0803@naver.com>
2024-09-05 01:06:04 +0900

    feat: EmptyBlankView mt 추가

--- apps/web/app/components/EmptyBlankView.tsx
+++ apps/web/app/components/EmptyBlankView.tsx
@@ -8,10 +8,10 @@ type Props = {
 
 const EmptyBlankView = ({ title, description, children }: Props) => {
   return (
-    <div className="flex flex-col gap-[4px] items-center">
-      <div className="w-[100px] h-[100px] bg-slate-50" />
-      {title && <p className="text-center text-white text-xl font-semibold ">{title}</p>}
-      {description && <p className="text-center text-[#919191] text-base font-medium ">{description}</p>}
+    <div className="mt-[200px] flex flex-col items-center gap-[4px]">
+      <div className="h-[100px] w-[100px] bg-slate-50" />
+      {title && <p className="mt-[25px] text-center text-xl font-semibold text-white">{title}</p>}
+      {description && <p className="text-center text-base font-medium text-[#919191]">{description}</p>}
       {children && children}
     </div>
   );
