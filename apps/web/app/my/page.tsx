a8a76fbded56314bb5841a14a7f8de5e0c380573
Eunbin Kang <kub0803@naver.com>
2024-09-18 19:51:18 +0900

    feat: 회원가입페이지 API 연동

--- apps/web/app/my/page.tsx
+++ apps/web/app/my/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 import { getMyProfile } from '@repo/services';
+
+import { PATH } from 'constant/path';
 
 export async function generateMetadata(): Promise<Metadata> {
   const data = await getMyProfile();
@@ -16,13 +19,15 @@ const MyProfilePage = async () => {
   const data = await getMyProfile();
 
   return (
-    <section className="layout center flex max-w-[952px] flex-col gap-[64px]">
+    <section className="layout center flex max-w-[952px] flex-col gap-[64px] pb-[140px] pt-[84px]">
       <div className="flex gap-[24px]">
         <div className="h-[150px] w-[150px] rounded-full bg-[#d9d9d9]" />
         <div className="flex flex-col gap-[18px]">
           <div className="flex flex-col gap-[8px]">
             <p className="text-[28px] font-medium text-white">{data?.nickName}</p>
-            <div className="text-base font-normal text-[#cccccc]">남성 {data?.birthDate}년생</div>
+            <div className="text-base font-normal text-[#cccccc]">
+              {data?.gender ? '남성' : '여성'} {data?.birthDate}년생
+            </div>
             <div className="text-Orange50 text-lg font-medium">
               재평가한 콘텐츠 수 : {data?.statistics?.numRevaluations ?? 0}개
             </div>
@@ -80,7 +85,9 @@ const MyProfilePage = async () => {
         <button className="text-Gray60 inline-flex h-[52px] w-[343px] items-center justify-center gap-2.5 rounded-xl bg-[#444444] px-6 py-4 text-center text-[17px] font-medium">
           로그아웃하기
         </button>
-        <button className="text-Gray40 text-center text-xl font-medium">회원 탈퇴하기</button>
+        <Link href={PATH.WITHDRAW} className="text-Gray40 text-center text-xl font-medium">
+          회원 탈퇴하기
+        </Link>
       </div>
     </section>
   );
--- apps/web/app/sign-up/page.tsx
+++ apps/web/app/sign-up/page.tsx
@@ -1,14 +1,84 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+
+import { getSession, signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+import { ObjectSchema, object } from 'yup';
+
+import { deleteCookie, getCookie } from 'utils/cookie';
+
+import { YUP_SCHEMA } from 'constant/schema';
 
 import SignUpModalBody from './SignUpModalBody';
 
 const SignUpPage = () => {
-  const handleShowModal = () => {
-    SignUpModalBody;
+  const router = useRouter();
+
+  const schema: ObjectSchema<any> = object({
+    birthDate: YUP_SCHEMA.REQUIRED,
+    gender: YUP_SCHEMA.REQUIRED,
+  });
+
+  const {
+    handleSubmit,
+    register, // onChange 등의 이벤트 객체 생성
+  } = useForm({
+    defaultValues: {
+      gender: true,
+      birthDate: '',
+    },
+    resolver: yupResolver(schema),
+  });
+
+  const handleSignUp = async (data: any) => {
+    const provider = await getCookie('provider');
+    const accessToken = await getCookie('access_token');
+    const email = (await getCookie('email')) ?? '';
+
+    if (provider) {
+      router.replace('/');
+    }
+
+    const _data = {
+      ...data,
+      gender: data.gender === '남자' ? true : false,
+      email,
+      provider,
+    };
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/users`, {
+      method: 'POST',
+      body: JSON.stringify(_data),
+      headers: {
+        'oAuth-token': `${accessToken}`,
+        'Content-Type': 'application/json',
+      },
+    });
+    if (res.status === 200) {
+      signIn('kakao');
+      deleteCookie('access_token');
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      deleteCookie('access_token');
+    };
+  }, []);
+
+  const test = (data: any) => {
+    console.log(data);
   };
 
   return (
-    <form className="flex min-h-[calc(100vh-103px-165px)] flex-col items-center justify-center gap-[60px]">
+    <form
+      onSubmit={handleSubmit(handleSignUp, test)}
+      className="layout flex min-h-[calc(100vh-103px)] flex-col items-center justify-center gap-[60px] py-[74px]"
+    >
       <div className="flex flex-col items-center gap-[37px]">
         <img className="h-[170px] w-[211.56px]" src="https://via.placeholder.com/212x170" />
         <div className="flex flex-col items-center justify-start gap-3">
@@ -25,19 +95,21 @@ const SignUpPage = () => {
           <div className="flex items-center gap-[30px]">
             <span className="w-14 text-[15px] font-semibold leading-relaxed text-white">출생연도</span>
             <input
+              {...register('birthDate')}
               className="text-Gray60 flex h-[34px] w-[260px] items-center border border-[#444444] px-2.5 py-2 text-[13px] font-medium leading-[18px]"
               placeholder="숫자로 4글자 입력해 주세요.(예: 2010)"
+              maxLength={4}
             />
           </div>
           <div className="flex items-center gap-[30px]">
             <span className="w-14 text-[15px] font-semibold leading-relaxed text-white">성별</span>
             <div className="flex items-center gap-10">
               <label className="flex items-center gap-2 text-center text-[13px] font-medium leading-[18px] text-white">
-                <input type="radio" name="sex" defaultChecked />
+                <input {...register('gender')} type="radio" defaultChecked value={'남자'} />
                 남자
               </label>
               <label className="flex items-center gap-2 text-center text-[13px] font-medium leading-[18px] text-white">
-                <input type="radio" name="sex" />
+                <input {...register('gender')} type="radio" value={'여자'} />
                 여자
               </label>
             </div>
