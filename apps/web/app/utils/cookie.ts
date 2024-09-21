6b70ebc927009b90b5d00e279c659bb24a970cde
Eunbin Kang <kub0803@naver.com>
2024-09-21 14:59:33 +0900

    feat: getCookie 제네릭타입 추가

--- apps/web/app/utils/cookie.ts
+++ apps/web/app/utils/cookie.ts
@@ -6,13 +6,13 @@ export async function setCookie(key: string, value: string) {
   cookies().set(key, value);
 }
 
-export async function getCookie(key: string) {
+export async function getCookie<T>(key: string) {
   const cookieData = cookies().get(key)?.value;
   return new Promise((resolve) =>
     setTimeout(() => {
       resolve(cookieData);
     }, 1000)
-  );
+  ) as T;
 }
 
 export async function deleteCookie(key: string) {
