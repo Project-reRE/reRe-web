28f66559d04bc6ee5e013c0946a217d30a8461a2
Eunbin Kang <kub0803@naver.com>
2024-09-23 21:32:15 +0900

    feat: MOVIE_SPECIAL_POINT_NAME 추가

--- packages/ui/src/SimpleBarChart.tsx
+++ packages/ui/src/SimpleBarChart.tsx
@@ -4,7 +4,23 @@ import React from 'react';
 
 import { Bar, BarChart, CartesianGrid, Cell, Label, LabelList, XAxis, YAxis } from 'recharts';
 
+import { MOVIE_SPECIAL_POINT_NAME, NumRank } from '@repo/services';
 import { ColorMap } from '@repo/tailwind-config/theme';
+
+const movieSpecialPointMap: { [key in MOVIE_SPECIAL_POINT_NAME]: string } = {
+  [MOVIE_SPECIAL_POINT_NAME.PLANNING_INTENT]: '기획 의도',
+  [MOVIE_SPECIAL_POINT_NAME.DIRECTORS_DIRECTION]: '감독의 연출',
+  [MOVIE_SPECIAL_POINT_NAME.ACTING_SKILLS]: '출연진 연기력',
+  [MOVIE_SPECIAL_POINT_NAME.SCENARIO]: '시나리오',
+  [MOVIE_SPECIAL_POINT_NAME.OST]: 'OST',
+  [MOVIE_SPECIAL_POINT_NAME.SOCIAL_ISSUES]: '음향적사회적 요소',
+  [MOVIE_SPECIAL_POINT_NAME.VISUAL_ELEMENT]: '시각적 요소',
+  [MOVIE_SPECIAL_POINT_NAME.SOUND_ELEMENT]: '음향적 이슈',
+};
+
+function getSpecialPointText(key: MOVIE_SPECIAL_POINT_NAME): string {
+  return movieSpecialPointMap[key];
+}
 
 const CustomXAxisTick = (props: any) => {
   const { x, y, payload } = props;
@@ -20,25 +36,19 @@ const CustomXAxisTick = (props: any) => {
   );
 };
 
-const SimpleBarChart = ({ data }: { data: [] }) => {
+const SimpleBarChart = ({ data }: { data: NumRank[] }) => {
   const COLORS = [ColorMap.Orange40, ColorMap.Green40, ColorMap.Cyan40];
-  const productSales = [
-    {
-      name: '감독의 연출',
-      number: '2',
-      product1: 500,
-    },
-    {
-      name: '배우의 연기력',
-      number: '3',
-      product1: 800,
-    },
-    {
-      name: '음악',
-      number: '1',
-      product1: 300,
-    },
-  ];
+
+  const productSales = data.map((el) => {
+    return { ...el, type: getSpecialPointText(el.type as MOVIE_SPECIAL_POINT_NAME) };
+  });
+
+  console.log(productSales, MOVIE_SPECIAL_POINT_NAME.ACTING_SKILLS, movieSpecialPointMap['시각적 요소']);
+
+  const maxLength =
+    data.reduce((max, item) => {
+      return item.value > (max?.value ?? 0) ? item : max;
+    }, data[0])?.value ?? 100;
 
   return (
     <div className="h-[318px] w-[368px] rounded-[14px] bg-neutral-800 py-[20px]">
@@ -54,7 +64,7 @@ const SimpleBarChart = ({ data }: { data: [] }) => {
       >
         <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#444444" />
         <XAxis
-          dataKey="name"
+          dataKey="type"
           interval={0}
           textAnchor="middle"
           tick={<CustomXAxisTick />}
@@ -62,19 +72,19 @@ const SimpleBarChart = ({ data }: { data: [] }) => {
           tickMargin={10}
         />
         <YAxis
-          domain={[0, 1000]}
+          domain={[0, maxLength]}
           tickCount={6}
           tick={{ fontSize: 10 }}
           tickLine={false}
           axisLine={false}
           tickMargin={10}
         />
-        <Bar dataKey="product1" maxBarSize={30} radius={[50, 50, 0, 0]}>
+        <Bar dataKey="value" maxBarSize={30} radius={[50, 50, 0, 0]}>
           {productSales.map((_, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} opacity={0.4} />
           ))}
           <Label />
-          <LabelList dataKey="product1" position={'top'} fontWeight={600} fontSize={11} opacity={1} />
+          <LabelList dataKey="value" position={'top'} fontWeight={600} fontSize={11} opacity={1} />
         </Bar>
       </BarChart>
     </div>
