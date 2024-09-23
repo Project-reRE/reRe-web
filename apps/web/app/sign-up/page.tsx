9b30894cb13f380ad6d398f1aa03a2ef2476a2cd
Eunbin Kang <kub0803@naver.com>
2024-09-23 21:31:51 +0900

    feat: 회원가입 로직 추가

--- apps/web/app/sign-up/page.tsx
+++ apps/web/app/sign-up/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { HTMLAttributes, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
-import { getSession, signIn } from 'next-auth/react';
+import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -11,12 +11,19 @@ import { ObjectSchema, object } from 'yup';
 
 import { deleteCookie, getCookie } from 'utils/cookie';
 
+import { LINK } from 'constant/link';
 import { YUP_SCHEMA } from 'constant/schema';
 
 import SignUpModalBody from './SignUpModalBody';
 
 const SignUpPage = () => {
   const router = useRouter();
+  const [selected, setSelected] = useState({
+    all: false,
+    age: false,
+    termsOfService: false,
+    privacyPolicy: false,
+  });
 
   const schema: ObjectSchema<any> = object({
     birthDate: YUP_SCHEMA.REQUIRED,
@@ -58,9 +65,11 @@ const SignUpPage = () => {
         'Content-Type': 'application/json',
       },
     });
+
     if (res.status === 200) {
-      signIn('kakao');
-      deleteCookie('access_token');
+      const provider = await getCookie<'kakao' | 'google' | 'apple'>('provider');
+      await signIn(provider);
+      await deleteCookie('access_token');
     }
   };
 
@@ -73,6 +82,18 @@ const SignUpPage = () => {
   const test = (data: any) => {
     console.log(data);
   };
+
+  const windowOpen = (url: string) => {
+    window.open(url);
+  };
+
+  // useEffect(() => {
+  //   if (selected.age && selected.privacyPolicy && selected.termsOfService) setSelected({ ...selected, all: true });
+  //   else setSelected({ ...selected, all: false });
+  // }, [selected]);
+
+  const isActive = selected.age && selected.privacyPolicy && selected.termsOfService;
+  const activeStyle = 'bg-[#c85a27] text-White' as HTMLAttributes<HTMLElement>['className'];
 
   return (
     <form
@@ -118,27 +139,78 @@ const SignUpPage = () => {
 
         <div className="flex flex-col gap-[16px]">
           <label className="flex items-center gap-[8px]">
-            <input type="checkbox" className="h-6 w-6 rounded border border-[#5e5e5e] bg-neutral-800" />
-            <span className="font-['Pretendard'] text-sm font-semibold text-[#b3b3b3]">모두 동의하기</span>
+            <input
+              type="checkbox"
+              className="h-6 w-6 rounded border border-[#5e5e5e] bg-neutral-800"
+              checked={selected.age && selected.privacyPolicy && selected.termsOfService}
+              onChange={() => {
+                if (selected.all) {
+                  setSelected({ all: false, age: false, termsOfService: false, privacyPolicy: false });
+                } else {
+                  setSelected({ all: true, age: true, termsOfService: true, privacyPolicy: true });
+                }
+              }}
+            />
+            <span className="text-sm font-semibold text-[#b3b3b3]">모두 동의하기</span>
           </label>
           <div className="h-[0px] w-[346px] border border-[#444444]" />
           <div className="flex flex-col gap-[12px]">
-            <label className="text-Gray60 flex w-fit items-center gap-[8px] text-sm font-normal leading-[18px]">
-              <input type="checkbox" className="h-6 w-6 rounded border border-[#5e5e5e] bg-neutral-800" />
+            <label className="text-Gray60 flex items-center gap-[8px] text-sm font-normal leading-[18px]">
+              <input
+                type="checkbox"
+                className="h-6 w-6 rounded border border-[#5e5e5e] bg-neutral-800"
+                checked={selected.age}
+                onChange={() => {
+                  setSelected({ ...selected, age: !selected.age });
+                }}
+              />
               (필수) 만 14세 이상이에요.
             </label>
-            <label className="text-Gray60 flex w-fit items-center gap-[8px] text-sm font-normal leading-[18px]">
-              <input type="checkbox" className="h-6 w-6 rounded border border-[#5e5e5e] bg-neutral-800" />
-              (필수) 서비스 이용약관 동의하기
-            </label>
-            <label className="text-Gray60 flex w-fit items-center gap-[8px] text-sm font-normal leading-[18px]">
-              <input type="checkbox" className="h-6 w-6 rounded border border-[#5e5e5e] bg-neutral-800" />
-              (필수) 개인정보 수집 및 이용 동의하기
-            </label>
+            <div className="text-Gray60 flex w-full items-center justify-between leading-[18px]">
+              <label className="flex items-center gap-[8px] text-sm font-normal">
+                <input
+                  type="checkbox"
+                  className="h-6 w-6 rounded border border-[#5e5e5e] bg-neutral-800"
+                  checked={selected.termsOfService}
+                  onChange={() => {
+                    setSelected({ ...selected, termsOfService: !selected.termsOfService });
+                  }}
+                />
+                (필수) 서비스 이용약관 동의하기
+              </label>
+              <button
+                className="text-sm font-normal leading-[18px] text-[#919191] underline"
+                onClick={() => windowOpen(LINK.termsOfService)}
+              >
+                보기
+              </button>
+            </div>
+            <div className="text-Gray60 flex w-full items-center justify-between leading-[18px]">
+              <label className="flex items-center gap-[8px] text-sm font-normal">
+                <input
+                  type="checkbox"
+                  className="h-6 w-6 rounded border border-[#5e5e5e] bg-neutral-800"
+                  checked={selected.privacyPolicy}
+                  onChange={() => {
+                    setSelected({ ...selected, privacyPolicy: !selected.privacyPolicy });
+                  }}
+                />
+                (필수) 개인정보 수집 및 이용 동의하기
+              </label>
+              <button
+                className="text-sm font-normal leading-[18px] text-[#919191] underline"
+                onClick={() => windowOpen(LINK.privacyPolicy)}
+              >
+                보기
+              </button>
+            </div>
           </div>
         </div>
       </div>
-      <button className="text-Gray60 flex h-[61px] w-[343px] items-center justify-center gap-2.5 rounded-xl bg-[#444444] px-6 py-4 text-2xl font-medium">
+      <button
+        disabled={!(selected.age && selected.privacyPolicy && selected.termsOfService)}
+        className={`text-Gray60 flex h-[61px] w-[343px] items-center justify-center gap-2.5 rounded-xl bg-[#444444] px-6 py-4 text-2xl font-medium ${isActive ? activeStyle : ''}`}
+      >
         회원 가입 및 로그인하기
       </button>
     </form>
